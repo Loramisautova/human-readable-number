478e5befe224aafef547182dec3daf4a007eab08
loramisautova <misautova.lora@gmail.com>
2022-08-11 17:38:31 +0300

    feat: add human readable number.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (num) {
+    const units = [
+        'zero',
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+        'ten',
+        'eleven',
+        'twelve',
+        'thirteen',
+        'fourteen',
+        'fifteen',
+        'sixteen',
+        'seventeen',
+        'eighteen',
+        'nineteen'
+    ];
+    const tens = [
+        'zero',
+        'ten',
+        'twenty',
+        'thirty',
+        'forty',
+        'fifty',
+        'sixty',
+        'seventy',
+        'eighty',
+        'ninety'
+    ];
+
+    if (num < 20) {
+        return units[num];
+    } else if (num < 100) {
+           if ((num % 10 === 0)) {
+               return tens[Math.floor(num / 10)];
+           } else {
+               return `${tens[Math.floor(num / 10)]} ${units[num % 10]}`
+           }
+    } else {
+        if ((num % 100 === 0)) {
+            return `${units[Math.floor(num / 100)]} hundred`;
+        } else {
+            return `${units[Math.floor(num / 100)]} hundred` + ' ' + toReadable([num % 100]);
+        }
+    }
 }
